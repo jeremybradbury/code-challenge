edb11e68db4f214eb3f5cd7c32565173452b0391
Jeremy Bradbury <jdbradbury@gmail.com>
2019-04-05 02:23:29 -0700

    Fix create & update endpoints

--- lorem-server/routes/users.js
+++ lorem-server/routes/users.js
@@ -4,10 +4,19 @@ var router = express.Router();
 
 router.post("/create", function(req, res) {
   models.MockData
-    .create({
-      email: req.body.email,
-      foo: req.body.foo,
-      bar: req.body.bar,
+    .findAll({
+      order: [["id", "DESC"]],
+      limit: 1,
+      plain: true,
+    })
+    .then(last => {
+      return models.MockData.create({
+        id: req.body.id || last.id + 1,
+        email: req.body.email,
+        message: req.body.message,
+        foo: req.body.foo,
+        bar: req.body.bar,
+      });
     })
     .then(function(result) {
       res.json(result);
@@ -21,15 +30,20 @@ router.get("/:id", function(req, res) {
 });
 
 router.post("/:id/update", function(req, res) {
+  let user = {
+    email: req.body.email || undefined,
+    message: req.body.message || undefined,
+    foo: req.body.foo || undefined,
+    bar: req.body.bar || undefined,
+  };
   models.MockData
-    .update({
-      id: req.params.id,
-      email: req.body.email,
-      foo: req.body.foo,
-      bar: req.body.bar,
+    .update(user, {
+      where: { id: req.params.id },
+      returning: true,
     })
-    .then(function(result) {
-      res.json(result);
+    .then(() => models.MockData.findByPk(req.params.id))
+    .then(data => {
+      res.json(data);
     });
 });
 
